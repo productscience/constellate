070d556d50ef845e5098716d69da36eabc94fdf6
Chris Adams <chris@productscience.co.uk>
2017-12-12 22:19:52 +0100

    Add sample script for creating user in firebase

---
+++ cl8-web/functions/createUserInFireBase.js
@@ -0,0 +1,18 @@
+const admin = require('firebase-admin')
+
+var serviceAccount = require('./service-account.json')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+})
+
+admin.auth().createUser({
+  uid: process.env.FIREBASE_USER_UID,
+  email: process.env.FIREBASE_USER_EMAIL
+}).then(function (userRecord) {
+    // See the UserRecord reference doc for the contents of userRecord.
+  console.log('Successfully created new user:', userRecord.uid)
+})
+  .catch(function (error) {
+    console.log('Error creating new user:', error)
+  })

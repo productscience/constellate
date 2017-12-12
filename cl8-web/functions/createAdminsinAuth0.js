32aa49d67e5ddba993b552ecabab5e9786696e2d
Chris Adams <chris@productscience.co.uk>
2017-12-12 22:12:50 +0100

    Add script for making a user an admin in auth0

---
+++ cl8-web/functions/createAdminsinAuth0.js
@@ -0,0 +1,36 @@
+
+var ManagementClient = require('auth0').ManagementClient;
+
+ // you get an auth0 token from
+ // https://manage.auth0.com/#/apis/management/explorer
+var auth0 = new ManagementClient({
+  token: process.env.AUTH0_TOKEN,
+  domain: process.env.AUTH0_DOMAIN
+});
+
+const adminUsers = process.env.AUTH0_ADMIN_USERS.split(',')
+
+adminUsers.forEach(function(email) {
+  makeAdminUser(email)
+})
+
+function makeAdminUser (emailAddress) {
+  auth0.getUsersByEmail(emailAddress)
+    .then(function (users) {
+      console.log(users)
+      return (users)
+    })
+    .then(function (users) {
+      let uid = users[0].user_id
+      let params = { admin: true }
+      updateUser(uid, params)
+    })
+}
+
+function updateUser(uid, params) {
+  auth0.updateAppMetadata({id: uid}, params)
+    .then(function (user) {
+      console.log('user updated')
+      console.log(user)
+    })
+}

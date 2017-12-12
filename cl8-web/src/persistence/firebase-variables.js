75529788d623091641d6f93a6c7199b57a0a96e5
Chris Adams <chris@productscience.co.uk>
2017-12-12 16:50:26 +0100

    Check in steps for safely handing env vara

---
+++ cl8-web/src/persistence/firebase-variables.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  functionsURL: process.env.FIREBASE_FUNCTIONSURL
+}

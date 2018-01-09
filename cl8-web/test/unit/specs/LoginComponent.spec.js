ae24be3fda2041fcb915eda836f9a376ae99850d
Chris Adams <chris@productscience.co.uk>
2018-01-09 15:49:36 +0100

    commit the failing test

--- cl8-web/test/unit/specs/LoginComponent.spec.js
+++ cl8-web/test/unit/specs/LoginComponent.spec.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import { mount } from 'vue-test-utils'
+import LoginComponent from '@/components/LoginComponent'
 
-// import LoginComponent from '@/components/LoginComponent'
+// importing this file, because imports EventEmitter, which is a class, causes
+// an error alone the lines of `SyntaxError: Unexpected token export`
+// how to I transpile it so jest doesn't choke on it?
 import FirebaseService from '@/persistence/FirebaseService'
-
-
-const fbase = new FirebaseService()
+//
+//
+// const fbase = new FirebaseService()
 
 
 describe('Login Component', () => {
   test('validSubmission', () => {
+    // test that when we have a working email, the form shows as valid
+
+
     // let wrapper = mount(LoginComponent, {
     //   propsData: { fbase: fbase }
     // })

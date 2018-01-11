e66b4586e0a0daa0aaedee53057aeaf592ef681f
Chris Adams <chris@productscience.co.uk>
2018-01-11 14:05:10 +0100

    WIP css tweaks to junk

--- cl8-web/src/components/LoginComponent.vue
+++ cl8-web/src/components/LoginComponent.vue
@@ -1,38 +1,43 @@
 <template>
-<div>
+<div class="w-100">
   <div role="status" aria-live="polite">
     </div>
   <form v-on:submit.prevent="signIn">
 
-    <div class="">
+    <div class="w-100 mb3">
+
+      <input type="text" name="email" v-model="email"
+        class="input-reset pa2 ba  mt1 w-50"
+        :class="{'bg-washed-red': errors && errors.has('email') }"
+        placeholder="your email address"
+        aria-label="your email address"
+        v-validate="'required|email'"
+        autocomplete="email" />
+
       <small v-if="errors && errors.has('email')"
-        class="help is-danger">
+        class="red ">
         {{ errors.first('email') }}
       </small>
+
     </div>
-    <input type="text" name="email" v-model="email"
-      class="input-reset pa2 ba mb3 mt1 w-70"
-      :class="{'bg-light-red': errors && errors.has('email') }"
-      placeholder="your email address"
-      aria-label="your email address"
-      v-validate="'required|email'"
-      autocomplete="email" >
 
-
-    <input type="password" name="password" v-model="password"
-      class="input-reset pa2 ba w-70"
-      placeholder="your password"
-      aria-label="your password" autocomplete="current-password">
+    <div class="w-100">
+      <input type="password" name="password" v-model="password"
+        class="input-reset pa2 ba w-50"
+        placeholder="your password"
+        aria-label="your password" autocomplete="current-password">
+    </div>
 
     <div class="mt2">
-      <button
-        class="f6 link dim br-pill ph3 pv2 mb2 dib bg-light-silver white w-50 ml4 mt2"
-        :class="{'bg-light-green': validity }"
-        :disabled="errors && errors.any()"
-        type="submit" name="button">
-        Sign in
-      </button>
+        <button
+          class="f6 link dim br-pill ph3 pv2 mb2 dib bg-light-silver white w-50 ml0 mt2"
+          :class="{'bg-light-green': validity }"
+          :disabled="errors && errors.any()"
+          type="submit" name="button">
+          Sign in
+        </button>
     </div>
+
 
   </form>
 
@@ -57,14 +62,14 @@ export default {
       //   email: this.email,
       //   password: this.password
       // }
-      console.log(this.$validator.fields)
+      // console.log(this.$validator.fields)
       // let loginResponse = this.fbase.handleAuthentication(user)
       // console.log(loginResponse)
     }
   },
   computed: {
     validity: function () {
-      console.log(this)
+      console.log(this.email)
     }
   }
 }

19366b35d2d0b2ea8be380b09695dd1e62017806
Chris Adams <chris@productscience.co.uk>
2018-01-12 22:08:01 +0100

    Remove unused auth0 and firebase service bits

--- cl8-web/src/auth/auth0-variables.js
+++
@@ -1,9 +0,0 @@
-export const AUTH_CONFIG = {
-  clientID: process.env.AUTH0_CLIENTID,
-  domain: process.env.AUTH0_DOMAIN,
-  redirectUri: process.env.AUTH0_REDIRECTURI,
-  audience: process.env.AUTH0_AUDIENCE,
-  apiUrl: 'API_IDENTIFIER',
-  responseType: 'token id_token',
-  scope: 'openid profile email email_verified'
-}

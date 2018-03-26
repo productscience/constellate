f5ce1f79e305fbbaababee83f8ea5cff618e955b
Chris Adams <chris@productscience.co.uk>
2018-03-26 14:08:46 +0200

    Remove the DGen mention
    We have more than one constellation, so this is wrong on the other ones

--- cl8-web/src/components/profile/ProfileComponent.vue
+++ cl8-web/src/components/profile/ProfileComponent.vue
@@ -80,7 +80,7 @@
     </div>
 
     <div class="referred-by f6 gray">
-      <p>Referred to the DGen Constellation by: Gavin Starks</p>
+      <p>Referred by: Gavin Starks</p>
     </div>
     </div>
 

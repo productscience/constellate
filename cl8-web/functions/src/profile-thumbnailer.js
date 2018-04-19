c42590ac0d600a180bf7fa5df45984557ba2427f
Chris Adams <chris@productscience.co.uk>
2018-04-19 11:22:16 +0200

    Add methods to add generated photos to profile
    Okay, I was totally querying wrong, before, which everything was such a pain.
    
    Because of the way data is stored with firebase, if have a persistent ID you might be referring to (like one for Airtable, for example), I now think it's a better idea to have first query to work out the pushkey of the profile you want, then fetch that profile by its push key.
    
    Definitely not the same as using SQL.

--- cl8-web/functions/src/profile-thumbnailer.js
+++ cl8-web/functions/src/profile-thumbnailer.js
@@ -1,9 +1,9 @@
 const spawn = require('child-process-promise').spawn
 const path = require('path')
 const os = require('os')
-const admin = require('firebase-admin')
-
 const debug = require('debug')('cl8.profiler-thumbnailer')
+
+const _ = require('lodash')
 module.exports = ProfileThumbnailer
 
 /**
@@ -15,12 +15,7 @@ module.exports = ProfileThumbnailer
  * @param {any} photoPath
  * @returns {Object}
  */
-function ProfileThumbnailer (config, profileId, photoPath) {
-  admin.initializeApp({
-    credential: admin.credential.cert(config.serviceAccount),
-    databaseURL: config.databaseURL
-  })
-
+function ProfileThumbnailer (admin, profileId, photoPath) {
   function updateProfile (profileId, photoPath) {
     // const prof = await fetchProfile(profileId)
 
@@ -31,17 +26,64 @@ function ProfileThumbnailer (config, profileId, photoPath) {
     return true
   }
 
-  function fetchProfile (profileId) {
+  /**
+   * Accepts a profileId, and finds the matching user. Returns a promise
+   * which resolves to a snapshot
+   *
+   * @param {String} profileId
+   * @returns {Promise} Snapshot
+   */
+  function fetchProfile (firebaseKey) {
     return admin
       .database()
       .ref('userlist')
-      .orderByChild('id')
-      .equalTo(profileId)
-      .once('value', person => {})
+      .child(firebaseKey)
+      .once('value')
+      .then(snap => snap)
+  }
+
+  /**
+   * Accepts a key to for a matchin a user list, long with the value,
+   * and returns the first matching key to use with fetchProfile()
+   *
+   * @param {String} lookupKey
+   * @param {String} lookupValue
+   * @returns {String} firebaseKey
+   */
+  function lookupProfile (lookupKey, lookupValue) {
+    debug('looking key: ', lookupKey, ' - with value:', lookupValue)
+    return admin
+      .database()
+      .ref('userlist')
+      .orderByChild(lookupKey)
+      .equalTo(lookupValue)
+      .limitToFirst(1)
+      .once('value')
+      .then(snap => {
+        return _.keys(snap.val())[0]
+      })
+  }
+  /**
+   * Accept a profile, and update the
+   *
+   * @param {any} profile
+   * @returns  {Promise} containing void
+   *
+   */
+  async function addPhotoUrls (profile, photoObject) {
+    // debug(profile)
+    const photos = await profile.child('photo')
+
+    // debug('photos ref', photos.ref)
+    const updatedPhotos = await photos.ref.set(photoObject)
+
+    return updatedPhotos
   }
 
   return {
     updateProfile,
-    fetchProfile
+    lookupProfile,
+    fetchProfile,
+    addPhotoUrls
   }
 }
--- cl8-web/functions/test/profile-thumbnailer.test.js
+++ cl8-web/functions/test/profile-thumbnailer.test.js
@@ -3,8 +3,13 @@ const debug = require('debug')('cl8.profile-thumbnailer.test')
 
 const serviceAccount = require('../service-account.json')
 const testData = require('./testdata.json')
-
 const fs = require('fs')
+const _ = require('lodash')
+const admin = require('firebase-admin')
+const initialisedApp = admin.initializeApp({
+  serviceAccount: serviceAccount,
+  databaseURL: 'https://munster-setup.firebaseio.com'
+})
 
 describe('profileThumbnailer', () => {
   const profileId = 'recj0EMy3sWHhdove'
@@ -14,21 +19,46 @@ describe('profileThumbnailer', () => {
   // const thumbnailUrls = generateThumbs(photoPath)
   // savedProf = addPhotoUrls(prof)
   // debug(serviceAccount)
-  const profThumber = ProfileThumbnailer(
-    {
-      serviceAccount: serviceAccount,
-      databaseURL: 'https://munster-setup.firebaseio.com'
-    },
-    testData
-  )
+  const profThumber = ProfileThumbnailer(initialisedApp, testData)
 
   test.skip('updateProfile - existing user', () => {})
   test.skip('updateProfile - no user', () => {})
 
-  test.only('fetchProfile - existing user', async () => {
-    const prof = await profThumber.fetchProfile(profileId)
+  test('lookupProfile - existing user', async () => {
+    // should look like -L5enfF49VrcTmKvXY05
 
-    debug(prof.val())
-    expect(prof.val().resolves.toHaveProperty('fields'))
+    const prof = await profThumber.lookupProfile('id', profileId)
+    debug('key looks like: ', prof)
+
+    // we're testing to see if this looks like a firebase key here
+    expect(prof).toMatch(/^-/)
+    expect(prof).toHaveLength(20)
+  })
+
+  test('fetchProfile - existing user', async () => {
+    const profileKey = await profThumber.lookupProfile('id', profileId)
+    const profile = await profThumber.fetchProfile(profileKey)
+
+    debug(profile.val())
+    expect(profile.val()).toHaveProperty('fields.photo')
+    expect(profile.val().fields.photo).toHaveLength(1)
+  })
+
+  test.only('addPhotoUrls - existing user', async () => {
+    const profileKey = await profThumber.lookupProfile('id', profileId)
+    const profile = await profThumber.fetchProfile(profileKey)
+
+    const photoUrls = {
+      large: 'http://someurl.com/large.jpg',
+      small: 'http://someurl.com/medium.jpg'
+    }
+
+    debug('profile', profile.val())
+    await profThumber.addPhotoUrls(profile, photoUrls)
+    // debug('updatedPhoto', updatedPhoto)
+    const newProfile = await profThumber.fetchProfile(profileKey)
+    debug('newProfile', newProfile.val())
+    expect(newProfile.val()).toHaveProperty('photo.large', photoUrls.large)
+    expect(newProfile.val()).toHaveProperty('photo.small', photoUrls.small)
   })
 })

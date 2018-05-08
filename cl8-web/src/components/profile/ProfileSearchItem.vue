54bde097b2d7e06e4e75c604b79c93a474934483
Chris Adams <chris@productscience.co.uk>
2018-05-08 13:35:36 +0200

    Remove the ugly blurring on the search result photos

--- cl8-web/src/components/profile/ProfileSearchItem.vue
+++ cl8-web/src/components/profile/ProfileSearchItem.vue
@@ -5,17 +5,19 @@
     class="list peep cf pa3 bb b--light-gray mid-gray hover-bg-white"
     @click="profileChosen">
 
-    <img
-      v-if="hasPhoto()"
-      :src="showPhoto('small')"
-      class='dib w-20'>
+    <div class="dib w-20">
 
-    <v-gravatar
-      v-else
-      :email="item.fields.email"
-      :size="64"
-      class="gravatar fl b--light-silver ba" />
+      <img
+        v-if="hasPhoto()"
+        :src="showPhoto('large')"
+        class='supplied-photo'>
 
+      <v-gravatar
+        v-else
+        :email="item.fields.email"
+        :size="64"
+        class="gravatar fl b--light-silver ba" />
+    </div>
     <div class="dib w-70 ph2 flex-auto v-top h3 h-auto-m overflow-hidden">
       <ul class="list pt1 mt0 ml0 pl0 pb1 f4-m">
         <li class="name mid-gray">{{ item.fields.name }}</li>

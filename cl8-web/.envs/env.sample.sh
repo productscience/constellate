c2d852fe4a764e9e3c8f3b84d226b98b0ecaf0b9
Chris Adams <chris@productscience.co.uk>
2018-09-17 09:32:00 +0200

    Fix typos in env var

--- cl8-web/.envs/env.sample.sh
+++ cl8-web/.envs/env.sample.sh
@@ -14,14 +14,14 @@ export FIREBASE_MESSAGINGSENDERID="1234567890123"
 
 # Used for cloud functions and admin scripts
 # This assumed you have downloaded a service-account json file at the path below, and named it accordingly
-export FIREBASE_SERVICE_ACCOUNT_PATH="$PROJECT_PATH/.envs/service-accounts/$FIREBASE_PROJECTID-service-account.json"
+export FIREBASE_SERVICE_ACCOUNT_PATH="$PROJECT_PATH/.envs/service-accounts/$FIREBASE_PROJECTID.service-account.json"
 
 ### Airtable
 
 # The user API key used for programatic access.
 # Best to grant read-only access to an account, then use that API key
 # instead of your own one
-export AIRTABLE_APIKEY="keyxxxxxxxxxxxxxx"
+export AIRTABLE_API_KEY="keyxxxxxxxxxxxxxx"
 
 # this identifies the airtable database we pull data from
 export AIRTABLE_BASE="appzxxxxxxxxxxxxx"

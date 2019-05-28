cccd8ac2f05d9e044ae695aa744d8f4eded367ff
Vincent Ahrend <mail@vincentahrend.com>
2019-05-28 20:25:39 +0200

    Update service-account.json location

--- cl8-web/server/src/config.js
+++ cl8-web/server/src/config.js
@@ -2,20 +2,20 @@ const admin = require('firebase-admin')
 
 /**
  * Return a firebase admin instance based on the service account file
- * at <project_root>/.envs/ and the FIREBASE_CONFIG env var
+ * at <project_root>/server/service-account.json and the FIREBASE_CONFIG env var
  */
 const setupFirebaseAdmin = () => {
-  if (process.env.FIREBASE_CONFIG == null)
-    console.log('Missing FIREBASE_CONFIG env var')
   const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG)
 
-  const serviceAccountPath = `../../.envs/${
-    adminConfig.projectId
-  }-service-account.json`
-  let serviceAccount = require(serviceAccountPath)
+  // const adminConfig = {
+  //   projectId: 'cl8-testing',
+  //   storageBucket: 'cl8-testing.appspot.com'
+  // }
+
+  let serviceAccount = require('../service-account.json')
 
   adminConfig.credential = admin.credential.cert(serviceAccount)
-  adminConfig.databaseURL = `${adminConfig.projectId}.firebaseio.com`
+  adminConfig.databaseURL = `cl8-testing.firebaseio.com`
   admin.initializeApp(adminConfig)
 
   return admin

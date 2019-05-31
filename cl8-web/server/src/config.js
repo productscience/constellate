02dc8d7c98703bc245481f493092bd1ab2515a24
Vincent Ahrend <mail@vincentahrend.com>
2019-05-31 18:27:44 +0200

    Fix firebase-admin config

--- cl8-web/server/src/config.js
+++ cl8-web/server/src/config.js
@@ -1,24 +1,28 @@
 const admin = require('firebase-admin')
 
 /**
- * Return a firebase admin instance based on the service account file
+ * Return a firebase admin instance
+ *
+ * For development environments this is based on the service account file
  * at <project_root>/server/service-account.json and the FIREBASE_CONFIG env var
  */
 const setupFirebaseAdmin = () => {
-  const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG)
+  if (process.env.NODE_ENV === 'production') {
+    // See server README.md for setup instructions
+    admin.initializeApp()
+    return admin
+  } else {
+    console.log('Loading config', process.env.FIREBASE_CONFIG)
+    const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG)
 
-  // const adminConfig = {
-  //   projectId: 'cl8-testing',
-  //   storageBucket: 'cl8-testing.appspot.com'
-  // }
+    let serviceAccount = require('../service-account.json')
 
-  let serviceAccount = require('../service-account.json')
+    adminConfig.credential = admin.credential.cert(serviceAccount)
+    // adminConfig.databaseURL = `cl8-testing.firebaseio.com`
+    admin.initializeApp(adminConfig)
 
-  adminConfig.credential = admin.credential.cert(serviceAccount)
-  adminConfig.databaseURL = `cl8-testing.firebaseio.com`
-  admin.initializeApp(adminConfig)
-
-  return admin
+    return admin
+  }
 }
 
 exports.default = setupFirebaseAdmin
